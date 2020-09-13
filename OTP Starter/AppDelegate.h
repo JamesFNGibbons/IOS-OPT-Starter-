b51d906dca48883c970f2d2ff31369f685d5503c
James Gibbons <jgibbons@flipweb.co.uk>
2020-09-13 21:00:48 +0100

    Initial Commit

---
+++ OTP Starter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OTP Starter
+//
+//  Created by James Gibbons on 13/09/2020.
+//  Copyright © 2020 James Gibbons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OTP Starter/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OTP Starter
+//
+//  Created by James Gibbons on 13/09/2020.
+//  Copyright © 2020 James Gibbons. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
